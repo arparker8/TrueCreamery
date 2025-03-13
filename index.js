d5939804eabe3b898b64250f722ebe8039c58a10
arparker8 <113953111+arparker8@users.noreply.github.com>
2025-03-13 15:20:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Previous Controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail Image Controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("flvr-img-slides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("flvr-img-caption");
+    
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
