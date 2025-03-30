687d7754803b7158bdfee677f098ef1db0af8667
arparker8 <113953111+arparker8@users.noreply.github.com>
2025-03-29 21:17:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/* 
+Ashleigh Parker - MGD2041
+
+True Creamery - Lactose Free Ice Cream Brand
+
+Document for all the Script that is being used for the project.
+*/
+
+/********** - Each Flavor Page - Slide Show - **********/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Previous Controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail Image Controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("flvr-img-slides");
+    let dots = document.getElementsByClassName("demo");
+    
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
