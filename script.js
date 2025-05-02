e633e8661bf3ba429c6ecaa3f7ca59499e7b1eab
arparker8 <113953111+arparker8@users.noreply.github.com>
2025-05-01 23:07:54 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,268 @@ True Creamery - Lactose Free Ice Cream Brand
 Document for all the Script that is being used for the project.
 */
 
+/********** - All Pages - Bag Pop Out - **********/
+
+// An Array of my Products so that it can be stored in localStorage.
+const products = [
+    {name: "Classic Cream", price: 12.00, photo: "./images/vanilla-img/packaging-vanilla.jpg"},
+    {name: "Simply Chocolate", price: 12.00, photo: "./images/chocolate-img/packaging-chocolate.jpg"},
+    {name: "Pure Strawberry", price: 12.00, photo: "./images/strawberry-img/packaging-strawberry.jpg"},
+    {name: "Cookies 'n Cream", price: 12.00, photo: "./images/cookies-img/packaging-cookies.jpg"},
+    {name: "Mint Chocolate Chip", price: 12.00, photo: "./images/mint-img/packaging-mint.jpg"},
+    {name: "Butter Pecan", price: 12.00, photo: "./images/pecan-img/packaging-pecan.jpg"}
+]
+
+const bagIcon = document.querySelector(".shop-cont");
+const bag = document.querySelector(".bag");
+const bagClose = document.querySelector("#bag-close");
+
+bagIcon.addEventListener("click", () => bag.classList.add("active"));
+bagClose.addEventListener("click", () => bag.classList.remove("active"));
+
+// This is for the Add to Bag buttons on my Flavors Page
+if (document.body.dataset.page === "our-flavors") {
+
+    // Button is connected to my Classic Vanilla Flavor product.
+    document.getElementById("add-btn-1").addEventListener("click", event => {
+        const flavor1 = products[0];
+        addToBag(flavor1);
+    });
+
+    // Button is connected to my Simple Chocolate Flavor product.
+    document.getElementById("add-btn-2").addEventListener("click", event => {
+        const flavor2 = products[1];
+        addToBag(flavor2);
+    });
+
+    // Button is connected to my Pure Strawberry Flavor product.
+    document.getElementById("add-btn-3").addEventListener("click", event => {
+        const flavor3 = products[2];
+        addToBag(flavor3);
+    });
+
+    // Button is connected to my Cookies 'n Cream Flavor product.
+    document.getElementById("add-btn-4").addEventListener("click", event => {
+        const flavor4 = products[3];
+        addToBag(flavor4);
+    });
+
+    // Button is connected to my Mint Chocolate Chip Flavor product.
+    document.getElementById("add-btn-5").addEventListener("click", event => {
+        const flavor5 = products[4];
+        addToBag(flavor5);
+    });
+
+    // Button is connected to my Butter Pecan Flavor Product.
+    document.getElementById("add-btn-6").addEventListener("click", event => {
+        const flavor6 = products[5];
+        addToBag(flavor6);
+    });
+
+};
+
+// This is for the Add to Bag button on Flavor 1 product page.
+if (document.body.dataset.page === "product-1") {
+
+    // Button is connected to my Classic Vanilla Flavor product.
+    document.getElementById("add-btn-1").addEventListener("click", event => {
+        const flavor1 = products[0];
+        addToBag(flavor1);
+    });
+};
+
+// This is for the Add to Bag button on Flavor 2 product page.
+if (document.body.dataset.page === "product-2") {
+
+    // Button is connected to my Simple Chocolate Flavor product.
+    document.getElementById("add-btn-2").addEventListener("click", event => {
+        const flavor2 = products[1];
+        addToBag(flavor2);
+    });
+};
+
+// This is for the Add to Bag button on Flavor 3 product page.
+if (document.body.dataset.page === "product-3") {
+
+    // Button is connected to my Pure Strawberry Flavor product.
+    document.getElementById("add-btn-3").addEventListener("click", event => {
+        const flavor3 = products[2];
+        addToBag(flavor3);
+    });
+};
+
+// This is for the Add to Bag button on Flavor 4 product page.
+if (document.body.dataset.page === "product-4") {
+    
+    // Button is connected to my Cookies 'n Cream Flavor product.
+    document.getElementById("add-btn-4").addEventListener("click", event => {
+        const flavor4 = products[3];
+        addToBag(flavor4);
+    });
+};
+
+// This is for the Add to Bag button on Flavor 5 product page.
+if (document.body.dataset.page === "product-5") {
+    
+    // Button is connected to my Mint Chocolate Chip Flavor product.
+    document.getElementById("add-btn-5").addEventListener("click", event => {
+        const flavor5 = products[4];
+        addToBag(flavor5);
+    });
+};
+
+// This is for the Add to Bag button on Flavor 6 product page.
+if (document.body.dataset.page === "product-6") {
+
+    // Button is connected to my Butter Pecan Flavor Product.
+    document.getElementById("add-btn-6").addEventListener("click", event => {
+        const flavor6 = products[5];
+        addToBag(flavor6);
+    });
+};
+
+const bagCont = document.querySelector(".bag-cont");
+let cart = JSON.parse(localStorage.getItem("cart") || "[]");
+
+// Function to add to cart.
+function addToBag(eachFlavor) {
+    const existingItem = cart.find(cart => cart.name === eachFlavor.name);
+    console.log(existingItem);
+
+    // If the items is already added this alert will be sent saying it's already in the Bag.
+    if (existingItem) {
+        alert("Item is already in the cart.")
+        return;
+    } else {
+        cart.push(eachFlavor);
+        displayItem(eachFlavor);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function displayItem(eachFlavor) {
+
+    // Creating the div with the items information for the Bag.
+    const bagBox = document.createElement("div");
+    let quantity = 1;
+    bagBox.classList.add("bag-box");
+    bagBox.innerHTML = `
+        <img src="${eachFlavor.photo}" class="bag-img">
+        <div class="bag-detail">
+            <h3>${eachFlavor.name}</h3>
+            <p class="bag-price">$${eachFlavor.price.toFixed(2)}</p>
+            <div class="bag-amount">
+                <div class="bag-quantity">
+                    <button id="decrement">-</button>
+                    <p class="number">${quantity}</p>
+                    <button id="increment">+</button>
+                </div>
+                <button class="bag-remove">Remove</button>
+            </div>
+        </div>
+    `;
+
+    bagCont.appendChild(bagBox);
+
+    // This is for when the Remove button is clicked.
+    bagBox.querySelector(".bag-remove").addEventListener("click", () => {
+        bagBox.remove();
+    
+        updateBagCount(-(quantity));
+    
+        updateSubtotalPrice();
+    
+        removeObjectByName("cart", eachFlavor.name);
+    
+    });
+    
+    // A function to connect remove Button and the localStorage.
+    function removeObjectByName(key, objectName) {
+        const storedArrayString = localStorage.getItem(key);
+        
+        if (storedArrayString) {
+            const storedArray = JSON.parse(storedArrayString);
+        
+            const filteredArray = storedArray.filter(obj => obj.name !== objectName);
+        
+            localStorage.setItem(key, JSON.stringify(filteredArray));
+        }
+    }
+    
+    // Changing the Quantity of the items when minus or plus buttons are clicked.
+    bagBox.querySelector(".bag-quantity").addEventListener("click", event => {
+        const numberElement = bagBox.querySelector(".number");
+    
+        if (event.target.id === "decrement" && quantity > 1) {
+            quantity--;
+    
+            updateBagCount(-1);
+    
+        } else if (event.target.id === "increment") {
+            quantity++;
+    
+            updateBagCount(1);
+        }
+    
+        numberElement.textContent = quantity;
+    
+        updateSubtotalPrice();
+    
+    });
+    
+    updateBagCount(1);
+    
+    updateSubtotalPrice();
+}
+
+// Adding together the Subtotal of the Items and Updating if things change.
+const updateSubtotalPrice = () => {
+    const totalPriceElement = document.querySelector(".subtotal-price");
+    const bagBoxes = bagCont.querySelectorAll(".bag-box");
+    let total = 0;
+
+    bagBoxes.forEach(bagBox => {
+        const priceElement = bagBox.querySelector(".bag-price");
+        const quantityElement = bagBox.querySelector(".number");
+        const price = priceElement.textContent.replace("$", "");
+        const quantity = quantityElement.textContent;
+        total += price * quantity; 
+    });
+
+    totalPriceElement.textContent = `$${total.toFixed(2)}`;
+}
+
+// Updating the Bag Icon Count.
+let bagItemCount = 0;
+const updateBagCount = change => {
+    const bagItemCountBadge = document.querySelector(".shop-item-count");
+    bagItemCount += change;
+    if (bagItemCount > 0) {
+        bagItemCountBadge.style.visibility = "visible";
+        bagItemCountBadge.textContent = bagItemCount;
+    } else {
+        bagItemCountBadge.style.visibility = "hidden";
+        bagItemCountBadge.textContent = "";
+    }
+}
+
+// Retrieve and display data (e.g., on page load)
+const storedData = localStorage.getItem("cart");
+const myArray = JSON.parse(storedData);
+
+if (myArray && myArray.length > 0) {
+    myArray.forEach(item => {
+        displayItem(item);
+    });
+}
+
 /********** - All Pages - Search Icon - **********/
 
 // When Search Icon is clicked a Search Box is Toggled to show.
 $(document).ready(function(){
 
-    $(".search").click(function(){
+    $(".search-icon").click(function(){
       $(".search-text-box").toggle();
     });
 });
@@ -37,7 +293,7 @@ document.getElementById("search-mobile").addEventListener("keydown", function(ev
 // The Menu Icon when clicked toggles to show below the header the Search and Menu Links.
 $(document).ready(function(){
 
-    $(".hamburger").click(function(){
+    $(".mobile-menu-icon").click(function(){
         $(".mobile-links").toggle();
     })
 })
@@ -119,7 +375,7 @@ timelines.forEach(timeline => {
     observer2.observe(timeline);
 })
 
-/********** - Our Story - Fade in on Scroll - **********/
+/********** - Our Story - Draw Line - **********/
 
 const observer3 = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -160,4 +416,108 @@ if (document.getElementById("contact-body")){
           $(el).val(finalVal);
         }
     }
+}
+
+/********** - Bag Page - Adding items to Check Out Page - **********/
+
+const checkoutCont = document.querySelector(".checkout-cont");
+
+function checkoutDisplay(eachFlavor) {
+        // Creating the div with the items information for the Bag.
+        const checkoutBox = document.createElement("div");
+        let quantity = 1;
+        checkoutBox.classList.add("checkout-box");
+        checkoutBox.innerHTML = `
+            <img src="${eachFlavor.photo}" class="checkout-img">
+            <div class="checkout-detail">
+                <div class="checkout-text">
+                    <h3>${eachFlavor.name}</h3>
+                    <p class="checkout-price">$${eachFlavor.price.toFixed(2)}</p>
+                </div>
+                <div class="checkout-amount">
+                    <div class="checkout-quantity">
+                        <button id="checkout-decrement">-</button>
+                        <p class="checkout-number">${quantity}</p>
+                        <button id="checkout-increment">+</button>
+                    </div>
+                    <button class="bag-remove">Remove</button>
+                </div>
+            </div>
+        `;
+    
+        checkoutCont.appendChild(checkoutBox);
+    
+        // This is for when the Remove button is clicked.
+        checkoutBox.querySelector(".bag-remove").addEventListener("click", () => {
+            checkoutBox.remove();
+        
+            updateBagCount(-(quantity));
+        
+            updateTotalPrice();
+        
+            removeObjectByName("cart", eachFlavor.name);
+        
+        });
+        
+        // A function to connect remove Button and the localStorage.
+        function removeObjectByName(key, objectName) {
+            const storedArrayString = localStorage.getItem(key);
+            
+            if (storedArrayString) {
+                const storedArray = JSON.parse(storedArrayString);
+            
+                const filteredArray = storedArray.filter(obj => obj.name !== objectName);
+            
+                localStorage.setItem(key, JSON.stringify(filteredArray));
+            }
+        }
+        
+        // Changing the Quantity of the items when minus or plus buttons are clicked.
+        checkoutBox.querySelector(".checkout-quantity").addEventListener("click", event => {
+            const numberElement = checkoutBox.querySelector(".checkout-number");
+        
+            if (event.target.id === "checkout-decrement" && quantity > 1) {
+                quantity--;
+        
+                updateBagCount(-1);
+        
+            } else if (event.target.id === "checkout-increment") {
+                quantity++;
+        
+                updateBagCount(1);
+            }
+        
+            numberElement.textContent = quantity;
+        
+            updateTotalPrice();
+        
+        });
+        
+        updateTotalPrice();
+}
+
+// Adding together the Subtotal of the Items and Updating if things change.
+function updateTotalPrice() {
+    const totalPriceElement = document.querySelector(".total-price");
+    const checkoutBoxes = checkoutCont.querySelectorAll(".checkout-box");
+    let total = 0;
+
+    checkoutBoxes.forEach(checkoutBox => {
+        const priceElement = checkoutBox.querySelector(".checkout-price");
+        const quantityElement = checkoutBox.querySelector(".checkout-number");
+        const price = priceElement.textContent.replace("$", "");
+        const quantity = quantityElement.textContent;
+        total += price * quantity; 
+    });
+
+    totalPriceElement.textContent = `$${total.toFixed(2)}`;
+}
+
+if (document.body.dataset.page === "bag") {
+    if (myArray && myArray.length > 0) {
+        myArray.forEach(item => {
+            checkoutDisplay(item);
+        });
+    }
+
 }
